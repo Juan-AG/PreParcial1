9dcad46bd77f7ed49784e5da88f711133df2a389
Juan David Alvarez Garcia <ldsjuan8@gmail.com>
2023-10-22 21:20:53 -0500

    Set up DbContext

---
+++ PreParcial/PreParcial/DAL/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PreParcial.DAL.Entities;
+
+namespace PreParcial.DAL
+{
+    public class DataBaseContext:DbContext
+    {
+        //Con este contructor me podré conectar a la BD, me brinda unas opciones de configuración que ya están definidas internamente
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique(); //Esto es un índice para evitar nombres duplicados de países
+          //  modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique(); //Indices Compuestos
+        }
+
+        public DbSet<Country> Countries { get; set; } //Esta línea me toma la clase Country y me la mappea en SQL SERVER para crear una tabla llamada COUNTRIES
+       // public DbSet<State> States { get; set; }
+        //Por cada nueva entidad que yo creo, debo crearle su DbSet
+    }
+}
